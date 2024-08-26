cffd697ba894e5f3d4b7ae80ff98845756f4f4fa
Ankit Gupta <iambittuankitbkg@gmail.com>
2024-08-26 21:27:08 +0530

    header 1 customized

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import logo from "../../jio_cinema.png";
 import { Link } from "react-router-dom";
-import { FaUserAlt } from "react-icons/fa";
+import { FaSearch, FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <nav className="header">
-      <img src={logo} alt="jio-cinema-logo" />
-      <div>
-        <Link to="/movies">Movies</Link>
-        <Link to="/sports">Sports</Link>
-        <Link to="/free">Free</Link>
-        <Link to="/premium">Premium</Link>
-      </div>
-      <FaUserAlt />
-    </nav>
+    <>
+      <nav className="header">
+        <div className="header-items">
+          <img src={logo} alt="jio-cinema-logo" />
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/movies">Movies</Link>
+            <Link to="/sports">Sports</Link>
+            <Link to="/free">Free</Link>
+            <Link to="/premium">Premium</Link>
+          </div>
+          <div className="search-bar">
+            <FaSearch />
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Movies, Shows and More"
+            />
+          </div>
+          <FaUserAlt />
+        </div>
+      </nav>
+      <nav className="header-2"></nav>
+    </>
   );
 };
 
